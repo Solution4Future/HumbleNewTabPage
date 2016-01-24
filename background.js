931b224685f56cd27d059fe33b9d43327960a062
Isaiah Billingsley <iza.billingsley@gmail.com>
2016-01-24 16:21:12 -0500

    Update opera startpage url

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-	if (tab.url.indexOf('opera://startpage') === 0) {
+chrome.tabs.onCreated.addListener(function (tab) {
+	if (/^(?:opera|browser):\/\/startpage\/?(?:speeddial)?/.test(tab.url)) {
 		chrome.tabs.update(tab.id, {
 			'url': 'newtab.html'
 		});
+		// chrome.tabs.create({
+		// 	index: tab.index,
+		// 	windowId: tab.windowId,
+		// 	url: 'newtab.html'
+		// });
+		// chrome.tabs.remove(tab.id);
 	}
 });
